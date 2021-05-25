9545679cedfb8dcf7f4aeead43e4f3029fead552
Nahla1020 <nahlahazmeh2000@gmail.com>
2021-05-25 13:25:39 +0300

    Project-initialization

---
+++ Application/Bluering/src/main/java/com/backend/Bluering/BlueringApplication.java
@@ -0,0 +1,13 @@
+package com.backend.Bluering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueringApplication.class, args);
+	}
+
+}
---
+++ Application/Bluering/src/test/java/com/backend/Bluering/BlueringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.Bluering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
